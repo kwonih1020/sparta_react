0619eadbf12b79ed3f394c8d73acf32a9c17b66f
Ick Hyun Kwon <kwonih1020@gmail.com>
2021-01-06 16:12:59 -0500

    added redux

--- bucket_list/src/App.js
+++ bucket_list/src/App.js
@@ -9,33 +9,46 @@ import styled from "styled-components";
 import Detail from "./Detail";
 import NotFound from "./NotFound";
 
+// 리덕스 스토어와 연결하기 위해 connect라는 친구를 호출할게요!
+import {connect} from 'react-redux';
+// 리덕스 모듈에서 (bucket 모듈에서) 액션 생성 함수 두개를 가져올게요!
+import {loadBucket, createBucket} from './redux/modules/bucket';
+
+// 이 함수는 스토어가 가진 상태값을 props로 받아오기 위한 함수예요.
+const mapStateTopProps = (state) => ({
+  bucket_list: state.bucket.list,
+});
+
+// 이 함수는 값을 변화시키기 위한 액션 생성 함수를 props로 받아오기 위한 함수예요.
+const mapDispatchToProps = (dispatch) => ({
+  load: () => {
+    dispatch(loadBucket());
+  },
+  create: (new_item) => {
+    console.log(new_item);
+    dispatch(createBucket(new_item));
+  }
+});
+
 // 클래스형 컴포넌트는 이렇게 생겼습니다!
 class App extends React.Component {
   constructor(props) {
     super(props);
     // App 컴포넌트의 state를 정의해줍니다.
     this.state = {
-      list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
+     
     };
     // ref는 이렇게 선언합니다!
     this.text = React.createRef();
   }
 
   componentDidMount() {
-    console.log(this.text);
+    console.log(this.props);
   }
 
   addBucketList = () => {
-    let list = this.state.list;
     const new_item = this.text.current.value;
-
-    // 리액트에서는 concat으로 배열항목을 합쳐주는 게 좋아요. 아래처럼요!
-    // list = list.concat(new_item);
-    // this.setState({list: list});
-
-    //  이건 가장 편한 배열 합치기 방법입니다.
-    //  ...은 배열 안에 있는 항목을 전부 꺼내서 늘어놓는다는 뜻입니다. (스프레드 문법이라고 불러요.)
-    this.setState({ list: [...list, new_item] });
+    this.props.create(new_item);
   };
 
   // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
@@ -49,15 +62,9 @@ class App extends React.Component {
           {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
           {/* Route 쓰는 법 2가지를 모두 써봅시다! */}
           <Switch>
-            <Route
-              path="/"
-              exact
-              render={(props) => <BucketList list={this.state.list} history={this.props.history} />}
-            />
-            <Route path="/detail" component={Detail} />
-            <Route render={(props) => (
-                <NotFound history={this.props.history} />
-            )} />
+            <Route path="/" exact component={BucketList} />
+            <Route path="/detail/:index" component={Detail} />
+            <Route component={NotFound} />
           </Switch>
         </Container>
         {/* 인풋박스와 추가하기 버튼을 넣어줬어요. */}
@@ -100,4 +107,5 @@ const Line = styled.hr`
   border: 1px dotted #ddd;
 `;
 // withRouter 적용
-export default withRouter(App);
+// connect로 묶어줬습니다!
+export default connect(mapStateTopProps, mapDispatchToProps)(withRouter(App));
--- bucket_list/src/index.js
+++ bucket_list/src/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+// import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom';
 
+// 우리의 버킷리스트에 리덕스를 주입해줄 프로바이더를 불러옵니다!
+import { Provider } from "react-redux";
+// 연결할 스토어도 가지고 와요.
+import store from "./redux/configStore";
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
